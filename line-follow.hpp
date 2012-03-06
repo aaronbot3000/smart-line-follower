90b2fbb5419597401d5f9b9d515a278f9abd57f4
Aaron Fan <aaron.y.fan@gmail.com>
2012-03-05 22:36:11 -0500

    whoops put prototypes in the wrong header

--- line-follow.hpp
+++ line-follow.hpp
@@ -12,4 +12,8 @@ using namespace cv;
 
 #define T_THRESH 20 //pixels
 
+bool track_one_line(Vector<Scalar_<float> > lines);
+// Returns false if we need to revert to one line tracking
+bool track_two_lines(Vector<Scalar_<float> > lines);
+
 #endif
